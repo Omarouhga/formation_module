18443786980165059f995978def045c581378cb1
omar ouhagua <omarouhga12@gmail.com>
2024-04-08 23:07:08 +0000

    licence

--- __manifest__.py
+++ __manifest__.py
@@ -5,6 +5,7 @@
     'category': 'Product',
     'depends': ['base','product','hr_contract'],
     'author': 'omar ouhagua',
+    'license': 'AGPL-3',
     'application': False,
     'data': [
         'views/product_view.xml',
